43cbcf2716f50c86f2282e0e8d6184af404be02d
elton <elton6103@gmail.com>
2025-05-31 10:29:17 -0300

    ✨ feat: feat: commit frontend project with Vue 3 and Vite

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <Header />
+    <Form />
+</template>
+
+<script setup>
+import Header from './components/Header.vue';
+import Form from './components/Form.vue';
+</script>
+
+<style></style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <h1>CadastroEmpresas</h1>
+    </header>
+
+    <section>
+        <h2>Cadastro de Empresa</h2>
+        <p>O primeiro passo para transformar a gestão da sua empresa começa aqui.</p>
+    </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+h1 {
+    color: var(--primary-color);
+    font-size: 3rem;
+}
+
+header {
+    margin: 2rem;
+}
+
+section {
+    text-align: center;
+    margin-top: 10rem;
+}
+
+section h2 {
+    font-size: 2.5rem;
+    margin-bottom: 2rem;
+}
+
+section p {
+    font-size: 1.6rem;
+    color: var(--secondary-color);
+    margin-bottom: 5rem;
+}
+</style>
