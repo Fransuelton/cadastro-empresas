c7acabc03991c894f630ff6e4803f9490a6b9c32
elton <elton6103@gmail.com>
2025-05-31 01:40:36 -0300

    ✨ feat: add 'nome' to fillable properties in Segmento model

--- app/Models/Segmento.php
+++ app/Models/Segmento.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Segmento extends Model
 {
+
+    protected $fillable = ['nome'];
+    
     public function empresas(): HasMany
     {
         return $this->hasMany(Empresa::class);
