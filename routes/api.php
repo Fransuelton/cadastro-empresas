6bdbc66932bd7726d763a6e5a8aeba9cd0041e72
elton <elton6103@gmail.com>
2025-05-31 01:55:14 -0300

    ♻️ refactor: reorder use statements in api.php for better organization

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SegmentoController;
+use App\Http\Controllers\EmpresaController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/segmentos', [SegmentoController::class, 'index']);
+
+Route::post('/empresas', [EmpresaController::class, 'store']);
