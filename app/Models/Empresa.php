682e846ac1bcfad7ca1801272ce1a108122ba6c5
elton <elton6103@gmail.com>
2025-05-31 01:54:30 -0300

    ✨ feat: define fillable properties in Empresa model

--- app/Models/Empresa.php
+++ app/Models/Empresa.php
@@ -8,6 +8,19 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Empresa extends Model
 {
+
+    protected $fillable = [
+        'nome',
+        'cep',
+        'rua',
+        'numero',
+        'bairro',
+        'estado',
+        'numero_whatsapp',
+        'cnpj_cpf',
+        'segmento_id'
+    ];
+
     public function segmentos(): BelongsTo
     {
         return $this->belongsTo(Segmento::class);
