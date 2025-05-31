a843e328cd90b640b2c4e174f86ea0ae42259f30
Fransuelton <elton6103@gmail.com>
2025-05-31 10:53:12 -0300

    🚑 hotfix: Delete backend directory

--- backend/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\SegmentoController;
-use App\Http\Controllers\EmpresaController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/segmentos', [SegmentoController::class, 'index']);
-
-Route::post('/empresas', [EmpresaController::class, 'store']);
